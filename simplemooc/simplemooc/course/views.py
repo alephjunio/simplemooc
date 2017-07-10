504407f1b55feef5a7c27c9bbbbae2cc46e61d91
alephjunio <alehjunio@gmail.com>
2017-07-10 15:07:09 -0300

    formulario de email atraves de console ok

--- simplemooc/simplemooc/course/views.py
+++ simplemooc/simplemooc/course/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 #importando o model de cursos para trabalhar com base de dados
 from .models import Course
-
-
+#importando formulario de cursos/duvidas
+from .forms import ContactCourse
 
 def index(request):
     #buscando todos os cursos
@@ -15,25 +15,24 @@ def index(request):
     }
     return render(request, templateName,context)
 
-
-#def details(request, pk):
-    ##buscando curso deacordo com sua chave primaria, caso não exista tranferir usuario para pagina de erro 404.
-    #course = get_object_or_404(Course, pk=pk)
-    ##definindo tamplete a ser renderizado
-    #templateName = 'courses/details.html'
-    ##dicionario de dados para passar para pagina de cursos
-    #context = {
-        #'course': course
-    #}
-    #return render(request, templateName,context)
-
 def details(request, slug):
     #buscando curso deacordo com sua chave primaria, caso não exista tranferir usuario para pagina de erro 404.
     course = get_object_or_404(Course, slug=slug)
+    #variavel de dicionario de dados
+    context = {}
+    #verificando se a uma request
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+        if form.is_valid():
+            context['is_valid'] = True
+            form.sendEmail(course,)
+            form = ContactCourse()
+    else:
+        form = ContactCourse()
+
     #definindo tamplete a ser renderizado
     templateName = 'courses/details.html'
     #dicionario de dados para passar para pagina de cursos
-    context = {
-        'course': course
-    }
+    context['form'] = form
+    context['course'] = course
     return render(request, templateName,context)
