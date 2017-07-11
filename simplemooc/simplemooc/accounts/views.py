02d648b1731262a2c3ee39ba5d16fc7a2abc803a
alephjunio <alehjunio@gmail.com>
2017-07-11 14:16:39 -0300

    adicionado novo campo em formulario de cadastro EMAIL

--- simplemooc/simplemooc/accounts/views.py
+++ simplemooc/simplemooc/accounts/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 
+from .forms import RegisterForm
+
 def register(request):
     #adicionando valor a variavel com camiho do template
     template_name = 'accounts/register.html'
     #verificando se o metodo de envio é post
     if request.method == 'POST':
         #atribuir dados em variavel form
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         print(form)
         #vereficando se é valido o formulario
         if form.is_valid():
@@ -16,7 +18,7 @@ def register(request):
             form.save()
             return redirect(settings.LOGIN_URL)
     context = {
-        'form' : UserCreationForm()
+        'form' : RegisterForm()
     }
 
     return render(request,template_name,context)
