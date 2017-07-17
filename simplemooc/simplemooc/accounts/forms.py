d09dd1b12e2a7c4aee475d2449e7a522bb66166b
alephjunio <alehjunio@gmail.com>
2017-07-17 15:33:03 -0300

    implementado customUser model

--- simplemooc/simplemooc/accounts/forms.py
+++ simplemooc/simplemooc/accounts/forms.py
@@ -1,44 +1,41 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 #realizar registro de usuarios
-class RegisterForm(UserCreationForm):
+class RegisterForm(forms.ModelForm):
 
         email = forms.EmailField(label='E-mail')
+        password1 = forms.CharField(label='Senha', widget=forms.PasswordInput)
+        password2 = forms.CharField(label='Confirmação de senha', widget=forms.PasswordInput)
 
-        # Verificar se email ja é existente em base de dados
-        def clean_email(self):
-            #capturar email
-            email = self.cleaned_data['email']
-            #consultar email.
-            if User.objects.filter(email=email).exists():
-                raise forms.ValidationError('Já existe usuáior com este E-mail')
-            return email
+        def clean_password2(self):
+            password1 = self.cleaned_data.get('password1')
+            password2 = self.cleaned_data.get('password2')
+            if password1 and password2 and password1 != password2:
+               raise forms.ValidationError('As senhas não são iguais')
+            return password2
+
 
 #travar transissão de save e capturar dados de email e apos isso continuar o commit em base de dados
         def save(self,commit=True):
                 user = super(RegisterForm,self).save(commit=False)
-                user.email = self.cleaned_data['email']
+                user.set_password(self.cleaned_data['password1'])
                 if commit:
                    user.save()
                 return user
+
+        class Meta:
+            model = User
+            fields = ['username','email']
 
 
 #realizar editar de usuarios
 class EditAccountForm(forms.ModelForm):
 
-    #consultar email para validar se já é existente em base de dados
-    def clean_email(self):
-        #capturar email
-        email = self.cleaned_data['email']
-        query_set = User.objects.filter(email=email).exclude(pk=self.instance.pk)
-        if query_set.exists():
-            raise forms.ValidationError('Já existe usuáior com este E-mail')
-        return email
-
     #informando quais campos podera ser editados pelo usuario
     class Meta:
         model = User
-        fields = ['username','email','first_name','last_name']
+        fields = ['username','email','name']
